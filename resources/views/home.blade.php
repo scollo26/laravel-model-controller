8a1d5c77b7da2d1e3ab1523d48ba9f735a4ae029
scollo26 <giovanniscollo26@gmail.com>
2022-02-17 15:40:48 +0100

    scss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,15 +2,17 @@
 
 @section('content')
 
-    
-        @foreach ($movies as $movie)
-        
+<div class=" justify-content-center d-flex flex-wrap  m-2">
+    @foreach ($movies as $movie)
+        <div class="card m-2  align-items-center justify-content-center">
             
-            <h1>{{$movie->title}}</h1> 
-        
-        @endforeach
+            <h1 class="card-title">{{ $movie->title }}</h1>
+            <h4>{{ $movie->original_title }}</h4>
+            <h5>{{ $movie->nationality }}</h5>
+            <h2>{{ $movie->date }}</h2>
+            <h2 class="vote">{{ $movie->vote }}</h2>
+        </div>
+    @endforeach
     
-            
-
-    
+</div>          
 @endsection
